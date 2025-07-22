2a979e268adcf2bac34943ee3f14b63140a0813d
purplerez <fairy.edensor@gmail.com>
2025-07-22 12:05:47 +0700

    fix : grades migration

--- database/migrations/2025_06_26_022856_create_grades_table.php
+++ database/migrations/2025_06_26_022856_create_grades_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('grades', function (Blueprint $table) {
             $table->id();
             $table->string('name', 10);
+            $table->foreignId('school_id')->constrained('schools');
             $table->timestamps();
         });
     }
