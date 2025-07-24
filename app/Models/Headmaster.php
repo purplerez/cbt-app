7b00fcb5ddb0ac44780a1edae159a1db505a4c36
purplerez <fairy.edensor@gmail.com>
2025-07-24 14:56:24 +0700

    add : headmaster model

--- app/Models/Headmaster.php
+++ app/Models/Headmaster.php
@@ -7,4 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 class Headmaster extends Model
 {
     //
+    protected $table = 'headmasters';
+
+    protected $fillable = [
+        'name',
+        'nip',
+        'email',
+        'phone',
+        'school_id',
+    ];
+
+    public function school()
+    {
+        return $this->belongsTo(School::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
