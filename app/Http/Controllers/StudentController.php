3d34b13261353cc0f7c3714bcec1ab23a9beafbb
purplerez <fairy.edensor@gmail.com>
2025-07-26 12:37:21 +0700

    minor add email_verified_at

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -52,6 +52,7 @@ class StudentController extends Controller
                 'password' => Hash::make($request->nis),
                 'role' => 'siswa', // Let the seeder explicitly set this to match Spatie role
                 // 'school_id' => $request->school_id
+                'email_verified_at' => now(),
             ]);
 
             // Assign student role using Spatie
@@ -76,12 +77,63 @@ class StudentController extends Controller
                 ->with('success', 'Data siswa berhasil ditambahkan');
         } catch (\Exception $e) {
             DB::rollBack();
+
+            // delete the uploaded photo if it exists and the transaction fails
+            if (isset($photoPath) && Storage::disk('public')->exists($photoPath)) {
+                Storage::disk('public')->delete($photoPath);
+            }
+
             // Delete the created user if it exists
             if (isset($user)) {
                 $user->delete();
             }
 
             return redirect()->back()->withInput()->withErrors(['error' => 'Input Failed : ' . $e->getMessage()]);
+        }
+    }
+
+    public function update(Request $request)
+    {
+        try{
+            $validatedData = $request->validate([
+                'id' => 'required|exists:students,id',
+                'nis' => 'required|unique:students,nis,' . $request->id,
+                'name' => 'required|string|max:255',
+                'grade_id' => 'required|exists:grades,id',
+                'gender' => 'required|in:L,P',
+                'p_birth' => 'required|string|max:255', // Place of birth
+                'd_birth' => 'required|date', // Date of birth
+                'address' => 'required|string',
+                'photo' => 'nullable|image|max:2048|mimes:jpeg,jpg,gif', // max 2MB and only JPEG, JPG, and GIF files
+            ]);
+            $student = Student::findOrFail($request->id);
+            $student->update([
+                'nis' => $validatedData['nis'],
+                'name' => $validatedData['name'],
+                'grade_id' => $validatedData['grade_id'],
+                'gender' => $validatedData['gender'],
+                'p_birth' => $validatedData['p_birth'],
+                'd_birth' => $validatedData['d_birth'],
+                'address' => $validatedData['address'],
+            ]);
+
+            if ($request->hasFile('photo')) {
+                $oldPhoto = $request->input('old_photo');
+                // Delete old photo if it exists and is not the default photo
+                if ($oldPhoto && $oldPhoto !== "assets/images/students/default.jpg") {
+                    Storage::disk('public')->delete($oldPhoto);
+                }
+                // Store the new photo
+                $imageName = $request->nis.'.'.$request->file('photo')->extension();
+                $photoPath = $request->file('photo')->storeAs('assets/images/students', $imageName, 'public');
+                $student->photo = $photoPath;
+                $student->save();
+            }
+
+            return redirect()->route('admin.schools.manage.view', session()->get('school_id'))->with('success', 'Data siswa berhasil diperbarui');
+        }
+        catch (\Exception $e) {
+            return redirect()->back()->withInput()->withErrors(['error' => 'Update Failed : ' . $e->getMessage()]);
         }
     }
 
@@ -94,18 +146,17 @@ class StudentController extends Controller
             if ($student->user) {
                 $student->user->delete();
             }
-
+            // Delete the photo if it exists and is not the default photo
+            if ($student->photo && $student->photo !== "assets/images/students/default.jpg") {
+                Storage::disk('public')->delete($student->photo);
+            }
             $student->delete();
 
-            return response()->json([
-                'success' => true,
-                'message' => 'Data siswa berhasil dihapus'
-            ]);
+            return redirect()->route('admin.schools.manage.view', session()->get('school_id'))
+                ->with('success', 'Data siswa berhasil dihapus');
         } catch (\Exception $e) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Terjadi kesalahan: ' . $e->getMessage()
-            ], 422);
+
+            return redirect()->back()->withErrors(['error' => 'Delete Failed : ' . $e->getMessage()]);
         }
     }
 }
