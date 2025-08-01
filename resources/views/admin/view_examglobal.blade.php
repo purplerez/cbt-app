1dc923964442f8ffc462f10368063af892b9b200
purplerez <fairy.edensor@gmail.com>
2025-08-01 14:39:12 +0700

    feat : view exams module

--- resources/views/admin/view_examglobal.blade.php
+++ resources/views/admin/view_examglobal.blade.php
@@ -10,12 +10,16 @@
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
 
-                    <a href="{{ route('admin.exams.create')}}" class="px-3 py-1.5 bg-green-600 text-white text-sm font-medium rounded hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-green-500 transition" >+ Tambah</a>    <x-input-error :messages="$errors->get('error')" class="mb-4" />
+                    <a href="{{ route('admin.exams.create')}}" class="px-3 py-1.5 bg-green-600 text-white text-sm font-medium rounded hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-green-500 transition" >
+                        + Tambah
+                    </a>
+                    <x-input-error :messages="$errors->get('error')" class="mb-4" />
                     <table class="min-w-full mt-4 text-sm text-left bg-white border border-gray-300 table-auto">
                         <thead class="text-gray-700 bg-gray-200">
                             <tr>
                                 <th class="px-4 py-2 border">No</th>
                                 <th class="px-4 py-2 border">Nama Sekolah</th>
+                                <th class="px-4 py-2 border">Status</th>
                                 <th class="px-4 py-2 border">Action</th>
                             </tr>
                         </thead>
@@ -23,14 +27,28 @@
                             @forelse ($exams as $exam)
                                 <tr>
                                     <td class="px-4 py-2 border">{{ $exam->id }}</td>
-                                    <td class="px-4 py-2 border">{{ $exam->name }}</td>
+                                    <td class="px-4 py-2 border">{{ $exam->title }}</td>
                                     <td class="px-4 py-2 border">
-                                        <a href="{{ route('admin.grades.edit', $exam->id) }}" class="btn btn-primary">Edit</a>
+                                        @if($exam->is_active == 'Y')
+                                            <span class="font-semibold text-green-600">Aktif</span>
+                                        @else
+                                            <span class="font-semibold text-red-600">Non-Aktif</span>
+                                        @endif
+                                    </td>
+
+                                    <td class="px-4 py-2 border">
+                                        <form action={{ route('admin.exams.manage',  $exam->id ) }} method="post" class="inline-block">
+                                            @csrf
+                                            <button type="submit" class="px-3 py-1.5 bg-blue-600 text-white text-sm font-medium rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition">
+                                                Manage
+                                            </button>
+                                        </form>
                                         <form action="{{ route('admin.grades.destroy', $exam->id) }}" method="POST" class="inline-block">
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit" class="px-3 py-1.5 bg-red-600 text-white text-sm font-medium rounded hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 transition">Delete</button>
                                         </form>
+
                                 </tr>
                             @empty
                                 <tr>
