26e57a26b36a1741ee5b5fe50f9859355a8b0a5a
purplerez <fairy.edensor@gmail.com>
2025-08-04 16:03:02 +0700

    add : question model

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -7,4 +7,34 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     //
+    protected $table = 'exam_questions';
+
+    protected $fillable = [
+        'exam_id',
+        'question_type_id',
+        'question_text',
+        'choices',
+        'answer_key',
+        'points',
+        'created_by',
+    ];
+
+    public function questionType(){
+        return $this->belongsTo(QuestionTypes::class);
+    }
+
+    public function exam(){
+        return $this->belongsTo(Examtype::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    /*
+    public function answers(){
+        return $this->hasMany(Answer::class);
+    }
+    */
+
 }
