6966308454607d089a5e45fd8d6906fb7f6f0ad1
purplerez <fairy.edensor@gmail.com>
2025-08-23 09:49:10 +0700

    fix : set nullable on examtype table as a form of global exam

--- database/migrations/2025_06_26_145723_create_exam_types_table.php
+++ database/migrations/2025_06_26_145723_create_exam_types_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             // $table->foreignId('exam_id')->constrained('exams');
-            $table->foreignId('school_id')->constrained('schools')->nullable();
-            $table->foreignId('grade_id')->constrained('grades')->nullable();
+            $table->foreignId('school_id')->nullable()->constrained('schools');
+            $table->foreignId('grade_id')->nullable()->constrained('grades');
             $table->dateTime('start_time');
             $table->dateTime('end_time');
             $table->boolean('is_active')->default(true);
