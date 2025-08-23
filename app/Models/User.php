4beade85cc0c2ff6a0e1bb3dc74dc9244fcdc728
purplerez <fairy.edensor@gmail.com>
2025-08-23 10:09:48 +0700

    api setting

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+// use Laravel\Sanctum\Contracts\HasApiTokens;
+use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
+
 
 class User extends Authenticatable
 {
