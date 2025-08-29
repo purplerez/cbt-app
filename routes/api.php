7362b0f3f8675da7a78fc0bbb3150a2a2a144cb7
purplerez <fairy.edensor@gmail.com>
2025-08-30 06:48:04 +0700

    add : auth sanctum and middleware in route api

--- routes/api.php
+++ routes/api.php
@@ -6,18 +6,24 @@ use App\Http\Controllers\API\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('exams', ExamController::class);
-Route::get('/schools/{schoolId}/students', [StudentController::class, 'getStudentsBySchool']);
-Route::post('/exams/add-student', [StudentController::class, 'addStudentToExam']);
-Route::get('/exams/{examId}/participants', [StudentController::class, 'getExamParticipants']);
-Route::get('/exams/{examId}/stats', [StudentController::class, 'getExamStats']);
+Route::middleware(['auth:sanctum', 'role:admin'])->prefix('admin')->name('admin.')->group(function() {
+    Route::apiResource('exams', ExamController::class);
+    Route::get('/schools/{schoolId}/students', [StudentController::class, 'getStudentsBySchool']);
+    Route::post('/exams/add-student', [StudentController::class, 'addStudentToExam']);
+    Route::get('/exams/{examId}/participants', [StudentController::class, 'getExamParticipants']);
+    Route::get('/exams/{examId}/stats', [StudentController::class, 'getExamStats']);
 
-// Student routes
-Route::get('/schools/{schoolId}/students', [StudentController::class, 'getStudentsBySchool']);
-Route::post('/exams/add-student', [StudentController::class, 'addStudentToExam']);
+    // Student routes
+    Route::get('/schools/{schoolId}/students', [StudentController::class, 'getStudentsBySchool']);
+    Route::post('/exams/add-student', [StudentController::class, 'addStudentToExam']);
 
-// logs
-Route::get('/exams/{userId}/Logs', [ExamlogController::class, 'getAllLogs']);
+    // logs
+    Route::get('/exams/{userId}/Logs', [ExamlogController::class, 'getAllLogs']);
+});
+Route::middleware(['auth:sanctum', 'role:siswa'])->prefix('siswa')->name('siswa.')->group(function() {
+    // Route::get('/dashboard', [SiswaController::class, 'index'])->name('dashboard');
+
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
