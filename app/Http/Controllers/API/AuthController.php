615872091a55394f8de99fb13355dd2fe83591f8
purplerez <fairy.edensor@gmail.com>
2025-08-30 07:32:43 +0700

    create auth controller

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+
 
 class AuthController extends Controller
 {
     //
+    public function login(Request $request){
+        $request->validate([
+            'nis' => 'required',
+            'password' => 'required',
+        ]);
+
+    }
 }
