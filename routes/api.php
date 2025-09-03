48bc277af6804f524eebdf7d9629ea0f00a81fe3
purplerez <fairy.edensor@gmail.com>
2025-09-03 15:18:09 +0700

    add : load questions endpoints

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 
 
-Route::middleware(['auth:sanctum', 'role:admin'])->prefix('admin')->name('admin.')->group(function() {
+// Route::middleware(['auth:sanctum', 'role:admin'])->prefix('admin')->name('admin.')->group(function() {
     Route::apiResource('exams', ExamController::class);
     Route::get('/schools/{schoolId}/students', [StudentController::class, 'getStudentsBySchool']);
     Route::post('/exams/add-student', [StudentController::class, 'addStudentToExam']);
@@ -24,12 +24,16 @@ Route::middleware(['auth:sanctum', 'role:admin'])->prefix('admin')->name('admin.
 
     // logs
     Route::get('/exams/{userId}/Logs', [ExamlogController::class, 'getAllLogs']);
-});
+//  });
 
-Route::middleware(['auth:sanctum', 'role:siswa'])->prefix('siswa')->name('siswa.')->group(function() {
+
+// peserta routes
+// Route::middleware(['auth:sanctum', 'role:siswa'])->prefix('siswa')->name('siswa.')->group(function() {
     Route::get('/dashboard', [ParticipantController::class, 'index'])->name('dashboard');
+    Route::post('/exams/{examId}/start', [ParticipantController::class, 'start'])->name('start');
+    // Route::post('/exams/{examId}/submit', [ParticipantController::class, 'submit'])->name('submit');
 
-});
+// });
 
 Route::get('/user', function (Request $request) {
     return $request->user();
