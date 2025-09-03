8f67d8cf356181ce506c3fb506ac007179a763e3
SeptianProject <septianuwoe75@gmail.com>
2025-09-03 21:50:38 +0700

    feat: setup project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
-export default function HomePage() {
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
+
+export default function LoginPage() {
+  const router = useRouter()
+
+  const handleLogin = () => {
+    router.push("/dashboard");
+  }
+
   return (
-    <div>
-      {/* Homepage content will be added later */}
+    <div className="flex flex-col items-center justify-center h-screen w-full">
+      <form action="" className="flex items-center justify-center flex-col gap-5">
+        <div className="flex flex-col items-center gap-3 w-full">
+          <Input placeholder="Email" />
+          <Input placeholder="Password" type="password" />
+        </div>
+        <div>
+          <Button onClick={handleLogin} className="font-heading text-white bg-blue-500 hover:bg-blue-600 px-8 text-sm font-medium rounded-md">Masuk</Button>
+        </div>
+      </form>
     </div>
   );
 }
