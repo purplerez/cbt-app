3f804be720876ca0f48baaafb9c185771608f45b
purplerez <fairy.edensor@gmail.com>
2025-09-17 08:48:28 +0700

    add : role restriction

--- resources/views/admin/manageperexam.blade.php
+++ resources/views/admin/manageperexam.blade.php
@@ -67,9 +67,16 @@
                                         Nilai
                                     </button>
                                     {{--  <button type="button" class="flex items-center w-full px-4 py-2 text-sm font-medium text-left text-white transition bg-red-600 rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"> --}}
-                                    <a href="{{ route('admin.exams.question.exit') }}"
-                                        class="flex items-center w-full px-4 py-2 text-sm font-medium text-left text-white transition bg-red-600 rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
-                                        title="Non-aktifkan Akun">
+                                        @role('admin')
+                                            <a href="{{ route('admin.exams.question.exit') }}"
+                                            class="flex items-center w-full px-4 py-2 text-sm font-medium text-left text-white transition bg-red-600 rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+                                            title="Non-aktifkan Akun">
+                                        @endrole
+                                        @role('super')
+                                            <a href="{{ route('admin.exams.question.exit') }}"
+                                            class="flex items-center w-full px-4 py-2 text-sm font-medium text-left text-white transition bg-red-600 rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+                                            title="Non-aktifkan Akun">
+                                        @endrole
                                         <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor"
                                             viewBox="0 0 24 24">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
