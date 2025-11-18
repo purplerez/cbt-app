f960f5b673c7354594918979746ea9528dcc7574
purplerez <fairy.edensor@gmail.com>
2025-11-18 22:39:27 +0700

    del : cetak button on view rooms

--- resources/views/kepala/view_rooms.blade.php
+++ resources/views/kepala/view_rooms.blade.php
@@ -38,9 +38,7 @@
                                     <td class="px-4 py-2 border">{{ $room->name }}</td>
                                     <td class="px-4 py-2 border">
 
-                                        <a href="{{--  --}}" class="px-3 py-1.5 mr-2 bg-green-600 text-white text-sm font-medium rounded hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 transition">
-                                            Cetak
-                                        </a>
+
 
                                         <a href="{{route('kepala.room.participants', $room->id)}}" class="px-3 py-1.5 mr-2 bg-white border border-blue-600 text-blue-600 text-sm font-medium rounded hover:bg-blue-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-500 transition">
                                             Peserta
