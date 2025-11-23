a873975ef21c7de7d8cc92e98546d1a486e75b1f
purplerez <fairy.edensor@gmail.com>
2025-11-24 00:10:40 +0700

    dell dd on questions

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -28,7 +28,7 @@ class QuestionController extends Controller
 
     public function store(Request $request)
     {
-        dd($request->all());
+        //dd($request->all());
         try {
             $choices = $request->input('choices', []);
             $answerKey = $request->input('answer_key', []);
