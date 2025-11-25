2138dce13c941f9cfca328189ed6420022ff3b75
purplerez <fairy.edensor@gmail.com>
2025-11-26 02:26:20 +0700

    fix : coloumn rekap nilai

--- resources/views/exports/exam-scores.blade.php
+++ resources/views/exports/exam-scores.blade.php
@@ -1,6 +1,6 @@
 <x-print-layout>
-    <div class="container mx-auto py-6">
-        <div class="text-center mb-6">
+    <div class="container py-6 mx-auto">
+        <div class="mb-6 text-center">
             <h1 class="text-2xl font-bold">Daftar Nilai Ujian</h1>
             <h2 class="text-xl">{{ $exam->name }}</h2>
             @if($school)
@@ -9,31 +9,30 @@
             @if($grade)
                 <p class="text-gray-600">Kelas {{ $grade->name }}</p>
             @endif
-            <p class="text-gray-600 mt-2">Total Skor Maksimal: {{ $totalPossibleScore }}</p>
+            <p class="mt-2 text-gray-600">Total Skor Maksimal: {{ $totalPossibleScore }}</p>
         </div>
 
         <table class="min-w-full border border-gray-300">
             <thead>
                 <tr>
-                    <th class="border border-gray-300 px-4 py-2">No</th>
-                    <th class="border border-gray-300 px-4 py-2">NIS</th>
-                    <th class="border border-gray-300 px-4 py-2">Nama Lengkap</th>
-                    <th class="border border-gray-300 px-4 py-2">Kelas</th>
-                    <th class="border border-gray-300 px-4 py-2">Nilai</th>
-                    <th class="border border-gray-300 px-4 py-2">Total Skor</th>
-                    <th class="border border-gray-300 px-4 py-2">Persentase</th>
+                    <th class="px-4 py-2 border border-gray-300">No</th>
+                    <th class="px-4 py-2 border border-gray-300">NIS</th>
+                    <th class="px-4 py-2 border border-gray-300">Nama Lengkap</th>
+                    <th class="px-4 py-2 border border-gray-300">Kelas</th>
+                    <th class="px-4 py-2 border border-gray-300">Nilai </th>
+
+                    <th class="px-4 py-2 border border-gray-300">Nilai Persentase</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach($scores as $index => $score)
                     <tr>
-                        <td class="border border-gray-300 px-4 py-2 text-center">{{ $index + 1 }}</td>
-                        <td class="border border-gray-300 px-4 py-2">{{ $score['nis'] }}</td>
-                        <td class="border border-gray-300 px-4 py-2">{{ $score['name'] }}</td>
-                        <td class="border border-gray-300 px-4 py-2">{{ $score['grade'] }}</td>
-                        <td class="border border-gray-300 px-4 py-2 text-right">{{ $score['score'] }}</td>
-                        <td class="border border-gray-300 px-4 py-2 text-center">{{ $score['score'] }}/{{ $score['total_possible'] }}</td>
-                        <td class="border border-gray-300 px-4 py-2 text-right">{{ $score['percentage'] }}%</td>
+                        <td class="px-4 py-2 text-center border border-gray-300">{{ $index + 1 }}</td>
+                        <td class="px-4 py-2 border border-gray-300">{{ $score['nis'] }}</td>
+                        <td class="px-4 py-2 border border-gray-300">{{ $score['name'] }}</td>
+                        <td class="px-4 py-2 border border-gray-300">{{ $score['grade'] }}</td>
+                        <td class="px-4 py-2 text-center border border-gray-300">{{ $score['score'] }}/{{ $score['total_possible'] }}</td>
+                        <td class="px-4 py-2 text-right border border-gray-300">{{ $score['percentage'] }}</td>
                     </tr>
                 @endforeach
             </tbody>
