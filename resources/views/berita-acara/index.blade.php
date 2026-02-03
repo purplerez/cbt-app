dda45a01e17df820dd9d0a7cc5d865538f10bd64
purplerez <fairy.edensor@gmail.com>
2026-02-03 23:02:12 +0700

    title berita acara download

--- resources/views/berita-acara/index.blade.php
+++ resources/views/berita-acara/index.blade.php
@@ -188,7 +188,7 @@
                                                     </a>
                                                 @endif
                                                 <a href="{{ route('kepala.berita-acara.pdf', $ba) }}"
-                                                   class="text-red-600 hover:text-red-900" title="Download PDF">
+                                                   class="text-red-600 hover:text-red-900" title="Berita Acara PDF" target="_blank">
                                                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>
                                                     </svg>
