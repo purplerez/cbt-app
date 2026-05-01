1c9459d7db2b07adf60ac37af35331a4627e1f61
Retno Ires Devina <fairy.edensor@gmail.com>
2026-05-01 20:22:52 +0700

    fix: typo

--- database/migrations/2025_08_01_204600_create_table_preassigned.php
+++ database/migrations/2025_08_01_204600_create_table_preassigned.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('preassigneds', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->consrtrained('users');
+            $table->foreignId('user_id')->constrained('users');
             $table->foreignId('exam_id')->constrained('exams');
             $table->timestamps();
         });
